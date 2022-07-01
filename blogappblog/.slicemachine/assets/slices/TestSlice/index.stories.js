3467b80d141c01115b71ae9cc581e1dc1b98d0c9
Demetris Demetriou <demetrisd25@gmail.com>
2022-07-02 00:06:51 +0300

    test

---
+++ blogappblog/.slicemachine/assets/slices/TestSlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/TestSlice';
+
+export default {
+  title: 'slices/TestSlice'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"test_slice","items":[],"primary":{"title":[{"type":"heading1","text":"Transition synergistic architectures","spans":[]}],"description":[{"type":"paragraph","text":"Cillum nulla eiusmod duis dolor occaecat ullamco mollit sit in mollit incididunt proident magna Lorem voluptate. Ullamco deserunt dolore mollit duis ut.","spans":[]}],"backgroundImage":{"dimensions":{"width":null,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1540206395-68808572332f?w=null&h=500&fit=crop"}},"id":"_Default"}} />
+_Default.storyName = 'Default'
