f02247ee9add7d4a57b333be44074a113977b5a8
Demetris Demetriou <demetrisd25@gmail.com>
2022-07-04 17:32:03 +0300

    test

--- blogappblog/pages/index.js
+++ blogappblog/pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -16,6 +17,7 @@ export default function Home() {
       
       </main>
 
+     
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
